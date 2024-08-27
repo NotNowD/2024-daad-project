244e003e262c7ea80a004b6167ce3c65a1fe8bf4
Danylo Kravchuk <danny.kr@ukr.net>
2024-08-27 22:00:55 +0300

    оновлено візуалізацію + вулканоплоти + збереження рисунків

--- package_install.R
+++ package_install.R
@@ -1,10 +1,19 @@
 
-install_if_missing <- function(package_name, bioconductor = FALSE) {
+install_if_missing <- function(package_name, bioconductor = FALSE, github_repo = NULL) {
   if (!requireNamespace(package_name, quietly = TRUE)) {
+    # Спробуйте встановити з CRAN або Bioconductor
     if (bioconductor) {
       BiocManager::install(package_name)
     } else {
       install.packages(package_name)
+    }
+    
+    # Якщо пакет все ще не встановлений, спробуйте встановити з GitHub
+    if (!requireNamespace(package_name, quietly = TRUE) && !is.null(github_repo)) {
+      if (!requireNamespace("devtools", quietly = TRUE)) {
+        install.packages("devtools")
+      }
+      devtools::install_github(github_repo)
     }
   }
   library(package_name, character.only = TRUE)
@@ -12,8 +21,12 @@ install_if_missing <- function(package_name, bioconductor = FALSE) {
 
 install_if_missing("readr")
 install_if_missing("tidyverse")
+install_if_missing("ggrepel")
+install_if_missing("patchwork")
+install_if_missing("pheatmap")
 install_if_missing("conflicted")
 install_if_missing("scales")
 install_if_missing("BiocManager")
 install_if_missing("recount3", bioconductor = TRUE)
 install_if_missing("DESeq2", bioconductor = TRUE)
+install_if_missing("EnhancedVolcano", github_repo = "kevinblighe/EnhancedVolcano")
