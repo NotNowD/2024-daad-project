25dbf18c3e2f9ed2914d3e09a22e90690ba9c217
Danylo Kravchuk <danny.kr@ukr.net>
2024-09-15 23:55:09 +0300

    all

--- package_install.R
+++ package_install.R
@@ -19,6 +19,7 @@ install_if_missing <- function(package_name, bioconductor = FALSE, github_repo =
   library(package_name, character.only = TRUE)
 }
 
+# Базові бібліотеки
 install_if_missing("readr")
 install_if_missing("tidyverse")
 install_if_missing("ggrepel")
@@ -27,7 +28,19 @@ install_if_missing("pheatmap")
 install_if_missing("conflicted")
 install_if_missing("scales")
 install_if_missing("BiocManager")
+
+# Bioconductor пакети
 install_if_missing("recount3", bioconductor = TRUE)
 install_if_missing("DESeq2", bioconductor = TRUE)
+
+# Інші пакети
 install_if_missing("ashr")
 install_if_missing("EnhancedVolcano", github_repo = "kevinblighe/EnhancedVolcano")
+
+# KEGG pathway аналіз
+install_if_missing("pathview", bioconductor = TRUE)
+install_if_missing("clusterProfiler", bioconductor = TRUE)
+
+# Допоміжні бібліотеки для роботи з анотаціями генів
+install_if_missing("org.Hs.eg.db", bioconductor = TRUE)
+install_if_missing("biomaRt", bioconductor = TRUE)
